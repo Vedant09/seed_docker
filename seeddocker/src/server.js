46d08af45667131be9f234615604d2dcbaaf44ce
Vedant09 <vedantvy98@gmail.com>
2024-04-27 23:18:11 -0400

    ghfg

--- seeddocker/src/server.js
+++ seeddocker/src/server.js
@@ -12,16 +12,6 @@ app.use(
 );
 app.use(bodyParser.json());
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.get("/profile-picture", function (req, res) {
-  let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
-  res.writeHead(200, { "Content-Type": "image/jpg" });
-  res.end(img, "binary");
-});
-
 // use when starting application locally
 let mongoUrlLocal = "mongodb://admin:pass@localhost:27017";
 
